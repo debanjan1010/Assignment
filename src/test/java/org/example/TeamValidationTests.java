830a6f63ea7b50adb42b3484bf3db0dceca8ff3a
debanjan1010@gmail.com <Guttu@12345>
2022-08-24 18:32:42 +0530

    Testng xml and surefire-plugin addition

--- src/test/java/org/example/TeamValidationTests.java
+++ src/test/java/org/example/TeamValidationTests.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TeamValidationTests {
+
+    @Test
+    public void demoTest(){
+        System.out.println("demotest method");
+        Assert.assertTrue(true, "This is demo test");
+    }
 }
