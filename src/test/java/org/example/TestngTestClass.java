22006e939a6c425be3395a7b971777ef8ce607d3
debanjan1010@gmail.com <Guttu@12345>
2022-08-24 16:30:25 +0530

    Maven project structure created with testng

---
+++ src/test/java/org/example/TestngTestClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TestngTestClass {
+
+    @Test
+    public void test1(){
+        System.out.println("This is testng test1");
+    }
+}
